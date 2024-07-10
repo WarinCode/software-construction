d4689945aca14c29b3798459e2e5cb71581a480a
WarinCode <warinsaipanya471@outlook.co.th>
2024-07-10 23:54:35 +0700

    added lab2 folder

---
+++ Lab02/ThaiCoins.java
@@ -0,0 +1,16 @@
+package Lab02;
+
+public class ThaiCoins {
+    public static int c10 = 0;
+    public static int c5 = 0;
+    public static int c2 = 0;
+    public static int c1 = 0;
+    public static void print(){
+        System.out.println(
+                "c10 = " + c10 + ",\n" +
+                "c5 = " + c5 + ",\n" +
+                "c2 = " + c2 + ",\n" +
+                "c1 = " + c1 + ",\n"
+        );
+    }
+}

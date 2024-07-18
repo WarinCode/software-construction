22166843b534f73223749c6ce4e2f8ca0f90f9f8
WarinCode <warinsaipanya471@outlook.co.th>
2024-07-18 12:52:48 +0700

    updated

---
+++ HW3/Birds.java
@@ -0,0 +1,23 @@
+package HW3;
+import java.util.Scanner;
+
+public class Birds {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        String sentence = sc.nextLine();
+        StringBuffer newSentence = new StringBuffer(sentence.toLowerCase());
+        int index = 0;
+
+        while(index != -1){
+            index = newSentence.indexOf("the");
+            if(index < 0 || (index + 3 ) > newSentence.length()){
+                break;
+            } else {
+                newSentence.delete(index, index + 3);
+                newSentence.insert(index, "***");
+            }
+        }
+
+        System.out.println(newSentence);
+    }
+}
---
+++ HW3/InputNames.java
@@ -0,0 +1,26 @@
+package HW3;
+import java.util.Scanner;
+
+public class InputNames {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String fullname = sc.nextLine();
+
+        int indexOfWhitespace = fullname.indexOf(" ");
+        String firstname = fullname.substring(0, indexOfWhitespace);
+        String lastname = fullname.substring(indexOfWhitespace + 1, fullname.length());
+
+        String temp = "";
+        for(int i = 0; i < lastname.length(); i++){
+            if(lastname.charAt(i) != ' '){
+                temp += lastname.charAt(i);
+            }
+        }
+        lastname = temp;
+
+        System.out.println("My name is " + firstname);
+        System.out.println("My lastname is " + lastname);
+    }
+}
